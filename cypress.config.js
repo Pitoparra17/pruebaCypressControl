92ce1c7b3ab454c9dce62d81fd3aab442473cd48
pitoparra17 <juanjoseparragomez@hotmail.com>
2025-09-05 04:35:18 -0500

    configuracion cypress y reporte con allure

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = defineConfig({
+  retries: 1,//realiza en el intento 1 y el intento 2
+  video: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+        allureWriter(on, config);
+        return config;
+    },
+  },
+});
